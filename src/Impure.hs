abd9830834238b36f504e3719cba4f6a99e50e63
drryner <drryner@icloud.com>
2018-11-29 16:43:16 +0500

    Add maybeCmd

--- src/Impure.hs
+++ src/Impure.hs
@@ -4,7 +4,12 @@ module Impure where
 import qualified Data.ByteString.Char8 as BS
 import Control.Exception (try, SomeException)
 import Data.ByteString.Char8 (ByteString)
+import System.Process (callCommand)
 
 maybeReadFileBS :: FilePath -> IO (Maybe ByteString)
 maybeReadFileBS path = (try $ BS.readFile path :: IO (Either SomeException ByteString))
+    >>= \res -> return $ either (const Nothing) Just res
+
+maybeCmd :: String -> IO (Maybe ())
+maybeCmd text = (try $ callCommand text :: IO (Either SomeException ()))
     >>= \res -> return $ either (const Nothing) Just res
