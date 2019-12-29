37f702d54b02ba4d95ab20651c82b33937378576
Ryner Gabriel <drryner@icloud.com>
2019-12-29 23:08:51 +0300

    Improve the help message

--- src/Config.hs
+++ src/Config.hs
@@ -14,4 +14,29 @@ defaultEquation = "default"
 
 -- help msg
 help :: String
-help = "https://github.com/DoctorRyner/sae"
+help = concat
+    [ "Checkout first https://github.com/DoctorRyner/sae#how-to-use\n\n"
+
+    , "In short, you need to create Equations.yaml, sae works similar to Makefile\n"
+    , "You need to specify formulas to run like 'sae app-build app-test'\n\n"
+
+    , "To create a new formula, you need to define it in Equations.yaml like this:\n\n"
+
+    , "    build: echo 'Building ... DONE!'\n\n"
+
+    , "Also, you can define the default formula like this:\n\n"
+
+    , "    default: sae build\n\n"
+
+    , "This formula would be run if you don't specify any formula\n\n"
+
+    , "You can run formulas asynchronously by using --async flag\n\n"
+
+    , "You can define constants and use them like this:\n\n"
+
+    , "    let:\n"
+    , "    - appName: My Awesome App\n"
+    , "    - appVersion: 0.4\n\n"
+
+    , "    greetings: Hi, I'm the creator of <appName> and it's my new <appVersion> version!\n"
+    ]
