11922d4dd8b03d9686fce0b71f142cfcd07e42d8
Ryner Gabriel <drryner@icloud.com>
2019-12-29 22:52:16 +0300

    Fix #123

--- src/Main.hs
+++ src/Main.hs
@@ -1,29 +1,28 @@
-{-# LANGUAGE LambdaCase #-}
 module Main where
 
 import qualified Config
+import           Control.Concurrent.Async (mapConcurrently_)
+import           Data.ByteString.Char8    (ByteString)
+import           Data.HashMap.Strict      (HashMap)
+import qualified Data.HashMap.Strict      as HashMap
+import           Data.Maybe               (fromJust)
+import           Data.Text                (Text, pack, unpack)
+import qualified Data.Vector              as Vector
+import           Data.Yaml                (Object, Value (..), decodeEither')
+import           Data.Yaml.Aeson          (Array)
 import qualified Error
-import qualified Data.Vector as Vector
-import qualified Data.HashMap.Strict as HashMap
-import Data.HashMap.Strict (HashMap)
-import Data.Text (Text, pack, unpack)
-import Data.Yaml (Object, Value(..), decodeEither')
-import Data.Yaml.Aeson (Array)
-import Data.Maybe (fromJust)
-import System.Environment (getArgs)
-import Data.ByteString.Char8 (ByteString)
-import Control.Concurrent.Async (mapConcurrently_)
-import Impure (maybeFileBS, exitIfCmdIsNotValid)
-import Regex.Parser (replace)
-import System.Exit (die)
+import           Impure                   (exitIfCmdIsNotValid, maybeFileBS)
+import           Regex.Parser             (replace)
+import           System.Environment       (getArgs)
+import           System.Exit              (die)
 
 -- if all provided equation names are valid then return list of equations content
 solveAll :: Object -> [String] -> Either String [String]
 solveAll equationsHash gottenFormulas = solve gottenFormulas [] where
     solve :: [String] -> [String] -> Either String [String]
     solve formulas solvedFormulas
-        | length formulas > 0 =
-            let headFormula        = pack $ head formulas 
+        | not $ null formulas =
+            let headFormula        = pack $ head formulas
                 maybeSolvedFormula = HashMap.lookup headFormula equationsHash
             in case maybeSolvedFormula of
                 Just (String solvedFormula) -> solve (tail formulas) $ solvedFormulas ++ [ unpack solvedFormula ]
@@ -35,7 +34,7 @@ resolveLets :: Array -> Either String (HashMap Text String)
 resolveLets gottenLets = resolve gottenLets HashMap.empty where
     resolve :: Array -> HashMap Text String -> Either String (HashMap Text String)
     resolve lets resolvedLets
-        | length lets > 0 = case Vector.head lets of
+        | not $ null lets = case Vector.head lets of
             Object val ->
                 let list = HashMap.toList val in
                 if length list == 1 then case head list of
@@ -52,19 +51,19 @@ resolveLets gottenLets = resolve gottenLets HashMap.empty where
         | otherwise = Right resolvedLets
 
 replaceLets :: [Text] -> String -> HashMap Text String -> String
-replaceLets letNames formula lets =
-    -- let possibleLets = getAll "@([^?]*)?" formula
-    -- in
-    foldr
-        (\letName out -> replace ("<" ++ letName ++ "\\>") out (fromJust $ HashMap.lookup (pack letName) lets)
-        ) formula $ map unpack letNames
+replaceLets letNames formula lets = foldr
+    ( (\letName out -> replace ("<" ++ letName ++ "\\>") out (fromJust $ HashMap.lookup (pack letName) lets)
+      ) . unpack
+    )
+    formula
+    letNames
 
 runCommands :: Bool -> [String] -> IO ()
 runCommands isAsync = (if isAsync then mapConcurrently_ else mapM_) exitIfCmdIsNotValid
 
 -- decode and parsing .yaml file
 yamlParse :: ByteString -> [String] -> Bool -> IO ()
-yamlParse equations formulas isAsync = case decodeEither' equations of 
+yamlParse equations formulas isAsync = case decodeEither' equations of
     Right (Object equationsHash) ->
         let maybeLets = HashMap.lookup (pack "let") equationsHash
         in case solveAll equationsHash formulas of
@@ -89,13 +88,12 @@ yamlParse equations formulas isAsync = case decodeEither' equations of
 
     Left  _ -> die Error.yamlIncorrectStructure
 
-run :: ByteString -> [String] -> IO ()
-run equations args = case length args of
+run :: [String] -> ByteString -> IO ()
+run args equations = case length args of
     0 -> yamlParse equations [ Config.defaultEquation ] False
 
     1 -> case firstArg of
         "--async" -> die Error.asyncKey
-        "--help"  -> putStrLn Config.help
         _         -> yamlParse equations [ firstArg ] False
 
     _ -> case firstArg of
@@ -105,5 +103,10 @@ run equations args = case length args of
   where firstArg = head args
 
 main :: IO ()
-main = maybeFileBS Config.fileToSolve >>= \case Just file -> run file =<< getArgs
-                                                Nothing   -> die Error.fileToSolve
+main = do
+    equations <- maybeFileBS Config.fileToSolve
+    args      <- getArgs
+
+    case args of
+        "--help":_ -> putStrLn Config.help
+        _          -> maybe (die Error.fileToSolve) (run args) equations
